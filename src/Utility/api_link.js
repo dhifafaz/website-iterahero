94c2a2cae3426a999fd9727e0300074fe09a5523
Rafi Arya Nugraha <toolop2312@gmail.com>
2022-11-24 15:16:53 +0700

    builda final harusnya

--- src/Utility/api_link.js
+++ src/Utility/api_link.js
@@ -1,51 +1,51 @@
-export const loginApi = "https://iterahero.fly.dev//api/v1/login";
-export const dashboardApi = "https://iterahero.fly.dev//api/v1/dashboard";
+export const loginApi = "https://iterahero.fly.dev/api/v1/login";
+export const dashboardApi = "https://iterahero.fly.dev/api/v1/dashboard";
 export const greenhouseByUserId =
-	"https://iterahero.fly.dev//api/v1/greenhouse";
+	"https://iterahero.fly.dev/api/v1/greenhouse";
 export const controllingApi =
-	"https://iterahero.fly.dev//api/v1/actuator?by_greenhouse_id=";
+	"https://iterahero.fly.dev/api/v1/actuator?by_greenhouse_id=";
 export const monitoringApi =
-	"https://iterahero.fly.dev//api/v1/sensor?by_id_greenhouse=";
+	"https://iterahero.fly.dev/api/v1/sensor?by_id_greenhouse=";
 export const listGreenhouse =
-	"https://iterahero.fly.dev//api/v1/greenhouse?size=100";
+	"https://iterahero.fly.dev/api/v1/greenhouse?size=100";
 export const addGreenhouse =
-	"https://iterahero.fly.dev//api/v1/greenhouse";
+	"https://iterahero.fly.dev/api/v1/greenhouse";
 export const deleteGreenhouse =
-	"https://iterahero.fly.dev//api/v1/greenhouse/";
+	"https://iterahero.fly.dev/api/v1/greenhouse/";
 export const updateGreenhouse =
-	"https://iterahero.fly.dev//api/v1/greenhouse/";
+	"https://iterahero.fly.dev/api/v1/greenhouse/";
 export const getApiGreenhouse =
-	"https://iterahero.fly.dev//api/v1/greenhouse/";
+	"https://iterahero.fly.dev/api/v1/greenhouse/";
 export const categoryApi =
-	"https://iterahero.fly.dev//api/v1/category/sensor";
-export const addSensorApi = "https://iterahero.fly.dev//api/v1/sensor";
-export const addActuatorApi = "https://iterahero.fly.dev//api/v1/actuator";
+	"https://iterahero.fly.dev/api/v1/category/sensor";
+export const addSensorApi = "https://iterahero.fly.dev/api/v1/sensor";
+export const addActuatorApi = "https://iterahero.fly.dev/api/v1/actuator";
 export const deleteAktuatorApi =
-	"https://iterahero.fly.dev//api/v1/actuator/";
-export const deleteSensorApi = "https://iterahero.fly.dev//api/v1/sensor/";
+	"https://iterahero.fly.dev/api/v1/actuator/";
+export const deleteSensorApi = "https://iterahero.fly.dev/api/v1/sensor/";
 export const paginationMonitoring =
-	"https://iterahero.fly.dev//api/v1/sensor?by_id_greenhouse=";
+	"https://iterahero.fly.dev/api/v1/sensor?by_id_greenhouse=";
 export const paginationAktuator =
-	"https://iterahero.fly.dev//api/v1/actuator?by_greenhouse_id=";
+	"https://iterahero.fly.dev/api/v1/actuator?by_greenhouse_id=";
 export const getNotificationByUserId =
-	"https://iterahero.fly.dev//api/v1/notification?by_user_id=1";
-export const brokerSensor = 'https://iterahero.fly.dev//api/v1/sensor_broker?id_sensor='
-export const idSensor =  'https://iterahero.fly.dev//api/v1/sensor/'
+	"https://iterahero.fly.dev/api/v1/notification?by_user_id=1";
+export const brokerSensor = 'https://iterahero.fly.dev/api/v1/sensor_broker?id_sensor='
+export const idSensor =  'https://iterahero.fly.dev/api/v1/sensor/'
 
-export const getGrafikSensor = 'https://iterahero.fly.dev//api/v1/grafik/'
+export const getGrafikSensor = 'https://iterahero.fly.dev/api/v1/grafik/'
 export const deleteNotification =
-	"https://iterahero.fly.dev//api/v1/notification/";
+	"https://iterahero.fly.dev/api/v1/notification/";
 
 export const updateActuatorDetail =
-	"https://iterahero.fly.dev//api/v1/actuator/";
+	"https://iterahero.fly.dev/api/v1/actuator/";
 
 
 
 export const updateSensorDetail =
-	"https://iterahero.fly.dev//api/v1/sensor/";
+	"https://iterahero.fly.dev/api/v1/sensor/";
 
-export const getLogAktuator = "https://iterahero.fly.dev//api/v1/actuator-log?by_actuator_id="
-export const postLogAktuator = 'https://iterahero.fly.dev//api/v1/actuator-log'
-export const icons = 'https://iterahero.fly.dev//api/v1/icon'
-export const Status = 'https://iterahero.fly.dev//api/v1/actuator-broker?id_actuator='
+export const getLogAktuator = "https://iterahero.fly.dev/api/v1/actuator-log?by_actuator_id="
+export const postLogAktuator = 'https://iterahero.fly.dev/api/v1/actuator-log'
+export const icons = 'https://iterahero.fly.dev/api/v1/icon'
+export const Status = 'https://iterahero.fly.dev/api/v1/actuator-broker?id_actuator='
 
